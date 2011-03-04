c2fecb294f1d36abfc4eaab28f6b6a12650d2a17
Joern Akkermann <joern@9thsky.info>
2011-03-04 16:01:05 +0100

    reversed changes; convert didn't worked so well

--- custom_font.rb
+++ custom_font.rb
@@ -4,28 +4,28 @@
 
 class CustomFont
   attr_reader :filename
-  
+
   PT_H1 = 45
   PT_H2 = 35
   PT_H3 = 25
   PT_P = 14
-  
+
   def self.pt_h1
     PT_H1
   end
-  
+
   def self.pt_h2
     PT_H2
   end
-  
+
   def self.pt_h3
     PT_H3
   end
-  
+
   def self.pt_p
     PT_P
   end
-    
+
   def self.pt_to_px(pt)
     (Float(96 * pt / 72)).round
   end
@@ -40,48 +40,62 @@ class CustomFont
     @url = url
     @filename = (@font + "_" + @text[0,30] + "_" + @pt.to_s.gsub(".","-") + "_" + @tag + ".jpg").gsub(/[\s]+/, "_")
   end
-  
+
   def url
     @url + self.filename
   end
-  
+
   def location
     @root_dir + self.filename
   end
-  
+
   def exists?
     File.exists?(self.location)
   end
-  
+
   def width
     CustomFont.pt_to_px(@pt) * @text.length / 2
   end
-  
+
   def height
     CustomFont.pt_to_px(@pt)
   end
-  
+
   def generate_image
     if !self.exists?
-      
+
       if !File.directory?(@root_dir)
         Dir.mkdir(@root_dir)
       end
-      
-      `convert -font '#{@font}' -pointsize #{@pt} label:'#{@text}' #{self.location}`
+
+      require "RMagick"
+
+      image = Magick::ImageList.new
+      image.new_image(self.width, self.height)
+
+      text = Magick::Draw.new
+      text.font = @font
+      text.pointsize = @pt
+      text.gravity = Magick::WestGravity
+
+      text.annotate(image, 0, 0, 0, 0, @text) {
+        self.fill = "black"
+      }
+
+      image.write(@root_dir + self.filename)
     end
-    
+
     self
   end
-  
+
   def to_html
     "<#{@tag} style=\"background: url('#{self.url}') no-repeat; width: #{self.width.to_s}px; height: #{self.height.to_s}px; text-indent: -9999px; overflow: hidden;\">#{@text}</#{@tag}>"
   end
-  
+
   def to_s
     self.generate_image.to_html
   end
-  
+
   def __destroy
     if @kill
       File.delete(self.location)
